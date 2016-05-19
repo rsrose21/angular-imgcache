a40e0ab3c9883b9682b6b0f16c1ee28d5a0b19ee
Ryan Rose <ryan.s.rose@ge.com>
2016-05-19 16:19:22 -0400

    Unbind watchers and test for defined values

--- angular-imgcache.js
+++ angular-imgcache.js
@@ -1,92 +1,95 @@
 angular.module('ImgCache', [])
 
-.provider('ImgCache', function() {
+    .provider('ImgCache', function() {
 
-    ImgCache.$init = function() {
+        ImgCache.$init = function() {
 
-        ImgCache.init(function() {
-            ImgCache.$deferred.resolve();
-        }, function() {
-            ImgCache.$deferred.reject();
-        });
-    }
-
-    this.manualInit = false;
-
-    this.setOptions = function(options) {
-        angular.extend(ImgCache.options, options);
-    }
-
-    this.setOption = function(name, value) {
-        ImgCache.options[name] = value;
-    }
-
-    this.$get = ['$q', function ($q) {
-
-        ImgCache.$deferred = $q.defer();
-        ImgCache.$promise = ImgCache.$deferred.promise;
-
-        if(!this.manualInit) {
-            ImgCache.$init();
+            ImgCache.init(function() {
+                ImgCache.$deferred.resolve();
+            }, function() {
+                ImgCache.$deferred.reject();
+            });
         }
 
-        return ImgCache;
-    }];
+        this.manualInit = false;
 
-})
+        this.setOptions = function(options) {
+            angular.extend(ImgCache.options, options);
+        }
 
-.directive('imgCache', ['ImgCache', function() {
+        this.setOption = function(name, value) {
+            ImgCache.options[name] = value;
+        }
 
-    return {
-        restrict: 'A',
-        scope: {
-            icBg: '@',
-            icSrc: '@'
-        },
-        link: function(scope, el, attrs) {
+        this.$get = ['$q', function ($q) {
 
-            var setImg = function(type, el, src) {
+            ImgCache.$deferred = $q.defer();
+            ImgCache.$promise = ImgCache.$deferred.promise;
 
-                ImgCache.getCachedFileURL(src, function(src, dest) {
-                    var img_src = dest.fullPath.replace('/'. ImgCache.options.localCacheFolder, ImgCache.getCacheFolderURI());
-                    if(type === 'bg') {
-                        el.css({'background-image': 'url(' + img_src + ')' });
-                    } else {
-                        el.attr('src', img_src);
+            if(!this.manualInit) {
+                ImgCache.$init();
+            }
+
+            return ImgCache;
+        }];
+
+    })
+
+    .directive('imgCache', ['ImgCache', function() {
+
+        return {
+            restrict: 'A',
+            scope: {
+                icBg: '@',
+                icSrc: '@'
+            },
+            link: function(scope, el, attrs) {
+
+                var setImg = function(type, el, src) {
+
+                    ImgCache.getCachedFileURL(src, function(src, dest) {
+                        el.addClass('img-cache-loaded');
+                        if(type === 'bg') {
+                            el.css({'background-image': 'url(' + dest + ')' });
+                        } else {
+                            el.attr('src', dest);
+                        }
+                    });
+                }
+
+                var loadImg = function(type, el, src) {
+
+                    ImgCache.$promise.then(function() {
+
+                        ImgCache.isCached(src, function(path, success) {
+
+                            if (success) {
+                                setImg(type, el, src);
+                            } else {
+                                ImgCache.cacheFile(src, function() {
+                                    setImg(type, el, src);
+                                });
+                            }
+
+                        });
+                    });
+                }
+
+                var unbindSrc = attrs.$observe('icSrc', function(src) {
+                    if (src) {
+                        loadImg('src', el, src);
+                        unbindSrc();
+                    }
+
+                });
+
+                var unbindBg = attrs.$observe('icBg', function(src) {
+                    if (src) {
+                        loadImg('bg', el, src);
+                        unbindBg();
                     }
                 });
+
             }
-
-            var loadImg = function(type, el, src) {
-
-                ImgCache.$promise.then(function() {
-
-                    ImgCache.isCached(src, function(path, success) {
-
-                        if (success) {
-                            setImg(type, el, src);
-                        } else {
-                            ImgCache.cacheFile(src, function() {
-                                setImg(type, el, src);
-                            });
-                        }
-
-                    });
-                });
-            }
-
-            attrs.$observe('icSrc', function(src) {
-
-                loadImg('src', el, src);
-
-            });
-
-            attrs.$observe('icBg', function(src) {
-
-                loadImg('bg', el, src);
-
-            });
-
-        }
-    };
-}]);
+        };
+    }]);
